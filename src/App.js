e828074319bd82385e2febe73b5cbe9aebc81b42
Joo <wl89818@gmail.com>
2024-09-13 11:35:18 +0900

    ✨Image
    - 이미지 종류
    - 이미지 종류에 따른 사용처
    - 이미지 import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import NewPayment from './components/NewPayment/NewPayment';
+import { within } from "@testing-library/react";
+import AlexaImage from "./images/alexa.png";
+import SiriImage from "./images/siri.png";
+
+console.log("AlexaImage", AlexaImage);
+console.log("SiriImage", SiriImage);
 
 function App() {
-
   return (
-    <NewPayment/>
+    <div>
+      <p>hello image</p>
+      <img src={AlexaImage} alt="alexa" width={500} height={500} />
+      <img src={SiriImage} alt="siri" style={{ width: 500, height: 500 }} />
+    </div>
   );
 }
 
