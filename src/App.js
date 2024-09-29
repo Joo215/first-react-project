dc6e1f1645123a2c2e436592de9cf066b5467ba4
Joo <wl89818@gmail.com>
2024-09-29 13:31:51 +0900

    Ref 실습

--- src/App.js
+++ src/App.js
@@ -1,59 +1,26 @@
-import { useState } from "react";
-import "./App.css";
-import PaymentForm from "./components/PaymentForm/PaymentForm";
-import Expenses from "./components/Payments/Expenses";
+import React, { useState, Fragment } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
-	const [expenses, setExpenses] = useState([
-		{
-			id: "e1",
-			title: "수건",
-			amount: 12.33,
-			date: new Date(2022, 3, 14),
-		},
-		{
-			id: "e2",
-			title: "물티슈",
-			amount: 234.11,
-			date: new Date(2023, 8, 22),
-		},
-		{
-			id: "e3",
-			title: "모니터",
-			amount: 3331.33,
-			date: new Date(2021, 10, 22),
-		},
-		{
-			id: "e4",
-			title: "의자",
-			amount: 298,
-			date: new Date(2022, 1, 31),
-		},
-		{
-			id: "e4",
-			title: "의자",
-			amount: 298,
-			date: new Date(2021, 6, 1),
-		},
-	]);
+  const [usersList, setUsersList] = useState([]);
 
-	const getPaymentFormData = (data) => {
-		setExpenses([
-			{
-				id: Math.random().toString(),
-				title: data.name,
-				amount: data.price,
-				date: new Date(data.today),
-			},
-		]);
-	};
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
 
-	return (
-		<>
-			<PaymentForm getPaymentFormData={getPaymentFormData} />
-			<Expenses items={expenses} />
-		</>
-	);
+  return (
+    <Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,68 @@
+import React, { useRef, useState } from "react";
+
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    console.log(nameInputRef.current.value);
+    console.log(ageInputRef.current.value);
+
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
+      setError({
+        title: "유효하지 않은 입력값",
+        message: "유효한 나이와 이름을 입력해주세요 (빈 값이 아닌).",
+      });
+      return;
+    }
+    if (+enteredUserAge < 1) {
+      setError({
+        title: "유효하지 않은 나이",
+        message: "유효한 나이를 입력해주세요 (> 0).",
+      });
+      return;
+    }
+    props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">사용자 이름</label>
+          <input id="username" type="text" ref={nameInputRef} />
+          <label htmlFor="age">나이</label>
+          <input id="age" type="number" ref={ageInputRef} />
+          <Button type="submit">사용자 추가</Button>
+        </form>
+      </Card>
+    </Wrapper>
+  );
+};
+
+export default AddUser;
