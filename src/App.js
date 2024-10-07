77660e10dacdaa2c68e89c7098588f38c0b1a945
Joo <wl89818@gmail.com>
2024-10-07 22:23:49 +0900

    ✨Post보내보기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import NewsList from './components/NewsList';
+import React from "react";
 
 function App() {
-  return (
-    <NewsList />
-  );
+  const postRequestHandler = async () => {
+    await fetch(
+      "https://react-test-eaf09-default-rtdb.firebaseio.com/test.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          key1: "value1",
+          key2: "value2",
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
+
+  return <button onClick={postRequestHandler}>Post</button>;
 }
 
 export default App;
